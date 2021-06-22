ac2a55b2791ea8a9768bf8d2419954080df516f0
Fabiana Santos <fabianasantosef@gmail.com>
2021-06-22 15:47:57 -0300

    criando o projeto

---
+++ src/main/java/br/com/trineo/hrtrabalho/HrTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.trineo.hrtrabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrTrabalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/trineo/hrtrabalho/HrTrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trineo.hrtrabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrTrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
