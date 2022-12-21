1bfec413261a7c76206bdfd6fb97ecd40f253874
fmmsantos <fabianasantosef@gmail.com>
2022-12-21 16:48:54 -0300

    Ajuste de comunicação entre microserviço

--- hr-folhaPagamento/src/main/java/br/com/trineo/hrfolhaPagamento/service/PagamentoService.java
+++ hr-folhaPagamento/src/main/java/br/com/trineo/hrfolhaPagamento/service/PagamentoService.java
@@ -1,24 +1,42 @@
 package br.com.trineo.hrfolhaPagamento.service;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Service;
-
 
 import br.com.trineo.hrfolhaPagamento.entity.Pagamento;
 import br.com.trineo.hrfolhaPagamento.entity.Trabalhador;
 import br.com.trineo.hrfolhaPagamento.feignClient.TrabalhadorFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PagamentoService {
 	
-	
+	@Value("${hr-trabalho.host}")
+	String hostRest;
 	@Autowired
 	private TrabalhadorFeignClient feign;
+	@Autowired
+	RestTemplate restTemplate;
 	
+	/*
 	public Pagamento getPagamento(Long idTrabalho, Integer dia ) {
-		
-		Trabalhador trabalhador = feign.findByTrabalhador(idTrabalho).getBody();
+		Map<String,String> idUrl = new HashMap<String,String>();
+		idUrl.put("id", ""+idTrabalho);
+		String nomeUri = hostRest + "/trabalhadores/{id}" + idTrabalho;
+		Trabalhador trabalhador = restTemplate.getForObject(hostRest + "/trabalhadores/{id}", Trabalhador.class, idUrl);
+		//Trabalhador trabalhador = feign.findByTrabalhador(idTrabalho).getBody();
 		return new Pagamento(trabalhador.getNome(),trabalhador.getValorDoTrabalho(),dia);
+
 	}
+	*/
+	public Pagamento getPagamento(Long idTrabalho, Integer dias) {
+		Trabalhador trabalhador = feign.findByTrabalhador(idTrabalho).getBody();
+		return new Pagamento(trabalhador.getNome(), trabalhador.getValorDoTrabalho(), dias);
+	}
+
 
 }
---
+++ hr-trabalho/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into trabalhador (nome,valor_do_trabalho) values ('bob', '300.00');
+insert into trabalhador (nome,valor_do_trabalho) values ('fabiana', '200.00');
+insert into trabalhador (nome,valor_do_trabalho) values ('marcio', '100.00');
